1f1227b9a57d84b9a9670137adc9ec609b6fa3cf
Avnit Bambah <msingh@eticainc.com>
2022-01-05 17:19:17 -0600

    updating the repo

---
+++ startup.bash
@@ -0,0 +1,7 @@
+pip install virtualenv
+virtualenv -v my-venv
+source my-venv/bin/activate
+pip install tda-api
+pip install selenium
+pip install webdriver-manager statsmodels matplotlib pandas_ta
+python authenticate.py 
